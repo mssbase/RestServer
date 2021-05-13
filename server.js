bc09de764a104c0850bcba7ce12f413344b3154a
Michal S <michalsienczuk@gmail.com>
2021-05-13 14:42:47 +0200

    add endpoints

--- server.js
+++ server.js
@@ -1,6 +1,49 @@
 const express = require('express');
+const db = require('./data');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/testimonials', (req, res) => {
+    res.json(db);
+});
+
+app.get('/testimonials/random', (req, res) => {
+    const id = Math.floor(Math.random() * db.length );
+    res.redirect(`/testimonials/${id}`);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    const record = db.find((el) => el.id === req.params.id);
+    const text = record.text;
+    res.json(text);
+});
+
+app.post('/testimonials', (req, res) => {
+    const { author, text } = req.body;
+    const newRecord = {};
+    newRecord.id = Math.random().toString(26).slice(2);
+    newRecord.author = author;
+    newRecord.text = text;
+    db.push(newRecord);
+    res.json({ message: "OK" });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+    const { author, text } = req.body;
+    const record = db.find((el) => el.id === req.params.id);
+    record.author = author;
+    record.text = text;
+    res.json({ message: "OK" });
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+    const record = db.find((el) => el.id === req.params.id);
+    const recordIndex = db.indexOf(record);
+    db.splice(recordIndex, 1);
+    res.json({ message: "OK" });
+});
 
 
 app.use((req,res) => {
